169fb0d3341de5488958cfd14422e507b73b97aa
Olga Romaniko <oromaniko@gmail.com>
2022-05-16 22:53:56 +0300

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React, {FC, useEffect, useState} from 'react';
-import AppRouter from "./components/AppRouter";
+import './App.css'
+import AppRouter from './components/AppRouter'
+import Navbar from './components/Navbar'
+import { Layout } from 'antd'
 
-const App:FC = () => {
-  return (
-    <div>
-      <AppRouter />
-    </div>
-  );
+const App = () => {
+    return (
+        <Layout>
+            <Navbar />
+            <Layout.Content>
+                <AppRouter />
+            </Layout.Content>
+        </Layout>
+    )
 }
 
-export default App;
+export default App
